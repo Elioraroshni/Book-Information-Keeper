fb9d25557d08b54b5c4e12d06b1d49f623a614cb
Elioraroshni <tanzimarahman13@gmail.com>
2019-10-09 00:34:19 -0700

    Add project files.

---
+++ Book Information Keeper/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Book_Information_Keeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.isbnRadioButton = new System.Windows.Forms.RadioButton();
+            this.nameRadioButton = new System.Windows.Forms.RadioButton();
+            this.isbnTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.addListBox = new System.Windows.Forms.ListBox();
+            this.searchListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Book";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(504, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Search Book";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "ISBN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Name";
+            // 
+            // isbnRadioButton
+            // 
+            this.isbnRadioButton.AutoSize = true;
+            this.isbnRadioButton.Location = new System.Drawing.Point(507, 111);
+            this.isbnRadioButton.Name = "isbnRadioButton";
+            this.isbnRadioButton.Size = new System.Drawing.Size(50, 17);
+            this.isbnRadioButton.TabIndex = 1;
+            this.isbnRadioButton.TabStop = true;
+            this.isbnRadioButton.Text = "ISBN";
+            this.isbnRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // nameRadioButton
+            // 
+            this.nameRadioButton.AutoSize = true;
+            this.nameRadioButton.Location = new System.Drawing.Point(658, 111);
+            this.nameRadioButton.Name = "nameRadioButton";
+            this.nameRadioButton.Size = new System.Drawing.Size(53, 17);
+            this.nameRadioButton.TabIndex = 1;
+            this.nameRadioButton.TabStop = true;
+            this.nameRadioButton.Text = "Name";
+            this.nameRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // isbnTextBox
+            // 
+            this.isbnTextBox.Location = new System.Drawing.Point(121, 113);
+            this.isbnTextBox.Name = "isbnTextBox";
+            this.isbnTextBox.Size = new System.Drawing.Size(195, 20);
+            this.isbnTextBox.TabIndex = 2;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(121, 141);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(195, 20);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(216, 181);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(100, 32);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(614, 181);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(97, 28);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(507, 141);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(204, 20);
+            this.searchTextBox.TabIndex = 2;
+            // 
+            // addListBox
+            // 
+            this.addListBox.FormattingEnabled = true;
+            this.addListBox.Location = new System.Drawing.Point(121, 252);
+            this.addListBox.Name = "addListBox";
+            this.addListBox.Size = new System.Drawing.Size(195, 173);
+            this.addListBox.TabIndex = 4;
+            this.addListBox.SelectedIndexChanged += new System.EventHandler(this.addListBox_SelectedIndexChanged);
+            // 
+            // searchListBox
+            // 
+            this.searchListBox.FormattingEnabled = true;
+            this.searchListBox.Location = new System.Drawing.Point(516, 252);
+            this.searchListBox.Name = "searchListBox";
+            this.searchListBox.Size = new System.Drawing.Size(195, 173);
+            this.searchListBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchListBox);
+            this.Controls.Add(this.addListBox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.isbnTextBox);
+            this.Controls.Add(this.nameRadioButton);
+            this.Controls.Add(this.isbnRadioButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Book Information Keeper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton isbnRadioButton;
+        private System.Windows.Forms.RadioButton nameRadioButton;
+        private System.Windows.Forms.TextBox isbnTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.ListBox addListBox;
+        private System.Windows.Forms.ListBox searchListBox;
+    }
+}
+
---
+++ Book Information Keeper/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Book_Information_Keeper
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<long, string> bookKeepingList = new Dictionary<long, string>();
+        List<long> isbnLIST = new List<long>();
+        List<string> nameLIST = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+          
+
+                    
+
+
+
+                string isbnNumber = isbnTextBox.Text;
+                long isbn = long.Parse(isbnNumber);
+                if (isbnNumber.Length != 13)
+                {
+                    MessageBox.Show("Insert valid isbn");
+                    isbnTextBox.Clear();
+                    nameTextBox.Clear();
+                }
+                else if (isbnNumber.Length == 13 && nameTextBox.Text.Length!=0)
+                {
+                    try
+                    {
+                        bookKeepingList.Add(isbn, nameTextBox.Text);
+
+                        addListBox.Items.Add(isbnTextBox.Text);
+                        addListBox.Items.Add(nameTextBox.Text);
+                    }
+                    catch (ArgumentException p)
+                    {
+                        MessageBox.Show("isbn ID alreday exist");
+                        isbnTextBox.Clear();
+                        nameTextBox.Clear();
+                    }
+                    isbnTextBox.Clear();
+                    nameTextBox.Clear();
+
+                }
+        }
+            
+        
+        
+
+        private void addListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            searchListBox.Items.Clear();
+            if (isbnRadioButton.Checked== true)
+            {
+
+             isbnLIST = bookKeepingList.Keys.ToList();
+             long i = long.Parse(searchTextBox.Text);
+                    if (isbnLIST.Contains(i))
+                    {
+                       searchListBox.Items.Add(bookKeepingList[i]);
+                       searchTextBox.Clear();
+                    }
+                    else 
+                    {
+                        MessageBox.Show("isbn number invalid");
+                        searchTextBox.Clear();
+                    }
+
+            }
+
+           else if (nameRadioButton.Checked == true)
+            {
+                nameLIST = bookKeepingList.Values.ToList();
+                string i = Convert.ToString(searchTextBox.Text);
+                if (nameLIST.Contains(i))
+                {
+                    searchListBox.Items.Add(searchTextBox.Text);
+                    searchTextBox.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("name invalid");
+                    searchTextBox.Clear();
+                }
+
+            }
+        }
+    }
+}
